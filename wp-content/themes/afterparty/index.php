0b620ca66ee0ef71a4d5ced6e075b9413a2fc805
juan2ramos <juan2ramos@gmail.com>
2015-11-25 00:24:02 -0500

    logo init

---
+++ wp-content/themes/afterparty/index.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Description: Home
+Theme: afterpartybogota
+*/
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php wp_title(' | '); ?></title>
+
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="<?php  bloginfo('template_url')?>/assets/images/favicon.ico">
+
+    <!---- TYPEKIT -- -->
+    <script type="text/javascript" src="http://use.typekit.com/uuf1qmf.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+    <link rel="stylesheet" href="<?php  bloginfo('template_url')?>/assets/css/normalize.css">
+    <link rel="stylesheet" href="<?php  bloginfo('template_url')?>/assets/css/style.css">
+
+
+
+</head>
+
+<body>
+
+<img src="<?php  bloginfo('template_url')?>/assets/images/after-party-Logo.png" alt="">
+
+</body>
+</html>
